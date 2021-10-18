9144eabe4a915010cf2b99a08d98c45d100611bd
Sophia Budrik <91380206+SophiaBud@users.noreply.github.com>
2021-10-18 22:51:59 +0300

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -2,16 +2,15 @@
 #include "Shape.h"
 
 /// <summary>
-/// Ęëŕńń äë˙ ęđóăŕ
+/// Класс для круга
 /// </summary>
 class Circle : public Shape 
 {
-
 private:
 	unsigned side = 0;
 public:
 	Circle();
-	Circle(double _diameter); // äčŕěĺňđ ęđóăŕ (ňŕęćĺ ĺăî řčđčíŕ č âűńîňŕ) = ďîëîâčíĺ đŕäčóńŕ
+	Circle(double _diameter); // диаметр круга (также его ширина и высота) = половине радиуса
 	double GetArea();
 	double GetPerimeter();
 };
